e53a6ffd8277c457cd17a6619a77969b87addfe8
Mlamali SAID SALIMO <saidsalimo@eisti.eu>
2021-04-22 16:09:13 +0200

    quitter fct

---
+++ src/application/AccueilController.java
@@ -0,0 +1,40 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.ButtonType;
+import javafx.scene.layout.AnchorPane;
+import javafx.stage.Stage;
+
+public class AccueilController {
+
+    @FXML
+    private Button btnAccueilEntrerBar;
+
+    @FXML
+    private Button btnAccueilGestionBar;
+
+    @FXML
+    private Button btnAccueilQuitter;
+
+    @FXML
+    private AnchorPane scenePan;
+
+    Stage stage;
+
+    public void quitterAppli(ActionEvent event) {
+
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quitter");
+        alert.setHeaderText("Etes vous sûre de vouloir quitter ?");
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            stage = (Stage) scenePan.getScene().getWindow();
+            System.out.println("Aurevoir !");
+            stage.close();
+        }
+
+    }
+
+}
---
+++ src/application/Main.java
@@ -0,0 +1,62 @@
+package application;
+
+import javafx.application.Application;
+import javafx.event.EventHandler;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+public class Main extends Application {
+
+    double xOffset, yOffset;
+
+    @Override
+    public void start(Stage stage) throws Exception{
+        Parent root = FXMLLoader.load(getClass().getResource("Accueil.fxml"));
+        Scene scene = new Scene(root, 1000, 600);
+
+        stage.setTitle("Bar à cocktails");
+
+        stage.setOnCloseRequest(event ->{
+            event.consume();
+            quitterAppli(stage);
+
+        });
+        // CSS
+       // String css = this.getClass().getResource("css/style.css").toExternalForm();
+        //scene.getStylesheets().add(css);
+
+        // Icon
+       // String icon = this.getClass().getResource("icon.png").toExternalForm();
+        //stage.getIcons().add(new Image(icon));
+
+
+        stage.show();
+
+
+    }
+
+    private void quitterAppli(Stage stage) {
+        Alert alert = new Alert(Alert.AlertType.CONFIRMATION);
+        alert.setTitle("Quitter");
+        alert.setHeaderText("Etes vous sûre de vouloir quitter ?");
+        if(alert.showAndWait().get() == ButtonType.OK) {
+            System.out.println("Aurevoir !");
+            stage.close();
+        }
+
+    }
+
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+}
